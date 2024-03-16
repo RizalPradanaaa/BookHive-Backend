1c476bf8ca87decfbb90a651b83c95a752a7bdb1
RizalPradanaaa <rizalpradanaaaa@gmail.com>
2024-03-16 11:17:28 +0700

    Add .env configuration

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
 import router from "./routes/BookRoute.js";
+
+dotenv.config();
+const Port = process.env.PORT;
 
 const app = express();
 
 app.use(router);
-app.listen(8080, () => console.log("App is listen in port 8080"));
+app.listen(Port, () => console.log(`Server is running on port ${Port}`));
