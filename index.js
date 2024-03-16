7d8dd1fdf89ea753dafbc66dba6208da75240e1c
RizalPradanaaa <rizalpradanaaaa@gmail.com>
2024-03-16 11:46:58 +0700

    Add Connection to mongodb

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import router from "./routes/BookRoute.js";
+import mongoose from "mongoose";
+
+const app = express();
+app.use(router);
 
 dotenv.config();
 const Port = process.env.PORT;
+// Membuat koneksi ke database MongoDB
+const { DB_HOST, DB_NAME, DB_USER, DB_PASS } = process.env;
+const dbUrl = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}=${DB_NAME}`;
 
-const app = express();
-
-app.use(router);
-app.listen(Port, () => console.log(`Server is running on port ${Port}`));
+mongoose
+  .connect(dbUrl)
+  .then(() => {
+    console.log("Connected to database");
+    // Setelah berhasil terhubung ke database, jalankan server Express
+    app.listen(Port, () => console.log(`Server is running on port ${Port}`));
+  })
+  .catch((error) => {
+    console.error("Error connecting to database:", error);
+  });
